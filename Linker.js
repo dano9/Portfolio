1f1c042a6fd4ec33f1c2cad07f7d8a60a8850e1a
dano9 <42714883+dano9@users.noreply.github.com>
2023-09-06 16:55:44 +0100

    Update Linker.js to check if user is on a mobile device

--- Linker.js
+++ Linker.js
@@ -1,3 +1,4 @@
+var isMobile = (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) != null;
 
 var time = 0;
 function Loop()
@@ -9,6 +10,7 @@ function Loop()
 }
 function OpenPage()
 {
+    console.log("isMobile:" + isMobile);
     PVStart();
     GameStart();
     Loop();
